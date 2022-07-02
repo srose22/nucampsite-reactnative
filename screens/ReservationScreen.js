25277c3b835c00ad7dc2439c4e5fa03ca5c446b9
Samantha Goulding <srgoulding@protonmail.com>
2022-07-02 12:32:18 -0400

    Task 2: Remove the Modal and add an Alert week 3

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -6,7 +6,7 @@ import {
   StyleSheet,
   Switch,
   Button,
-  Modal,
+  Alert,
   Platform,
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
@@ -18,7 +18,6 @@ const ReservationScreen = () => {
   const [hikeIn, setHikeIn] = useState(false);
   const [date, setDate] = useState(new Date());
   const [showCalendar, setShowCalendar] = useState(false);
-  const [showModal, setShowModal] = useState(false);
 
   const onDateChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
@@ -30,7 +29,20 @@ const ReservationScreen = () => {
     console.log("campers:", campers);
     console.log("hikeIn:", hikeIn);
     console.log("date:", date);
-    setShowModal(!showModal);
+    Alert.alert(
+      "Begin Search?",
+      `Number of Campers: ${campers} \nHike-In? ${hikeIn} \nDate: ${date.toLocaleDateString(
+        "en-US"
+      )}`,
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") },
+      ]
+    );
   };
 
   const resetForm = () => {
@@ -93,31 +105,6 @@ const ReservationScreen = () => {
             accessibilityLabel="Tap me to search for available campsites to reserve"
           />
         </View>
-        <Modal
-          animationType="slide"
-          transparent={false}
-          visible={showModal}
-          onRequestClose={() => setShowModal(!showModal)}
-        >
-          <View style={styles.modal}>
-            <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-            <Text style={styles.modalText}>Number of Campers: {campers}</Text>
-            <Text style={styles.modalText}>
-              Hike-In?: {hikeIn ? "Yes" : "No"}
-            </Text>
-            <Text style={styles.modalText}>
-              Date: {date.toLocaleDateString("en-US")}
-            </Text>
-            <Button
-              onPress={() => {
-                setShowModal(!showModal);
-                resetForm();
-              }}
-              color="#5637DD"
-              title="Close"
-            />
-          </View>
-        </Modal>
       </Animatable.View>
     </ScrollView>
   );
